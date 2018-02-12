7bb1f5a4738678bc9812f1a7a6fbef559730f3f1
Juhyun-Kim-Memphis <kjh.memphis@gmail.com>
2018-02-12 19:01:21 +0900

    [in the mid of develing CTHR] not running tests.

--- ControlThread.hpp
+++ ControlThread.hpp
@@ -2,20 +2,119 @@
 #define EVENT_MANAGER_CONTROLTHREAD_HPP
 
 #include <vector>
+#include <boost/asio.hpp>
+#include <iostream>
+#include "Pipe.hpp"
 
-class ClientConnection {
+using namespace boost;
 
+class ListeningSocket {
+public:
+    ListeningSocket(asio::io_service &ioService, unsigned short listeningPortNum)
+            : isStopped(false), ioService(ioService),
+              acceptor(ioService, asio::ip::tcp::endpoint(asio::ip::address_v4::any(), listeningPortNum)) {}
+
+    void start() {
+        acceptor.listen();
+        acyncAccept();
+    }
+
+    void stop() {
+        isStopped.store(true);
+    }
+
+private:
+    using Socket = asio::ip::tcp::socket;
+
+    void registerClient(const std::shared_ptr<Socket> &newClient) {
+        connectedClients.push_back(newClient);
+    }
+
+    void acyncAccept() {
+        std::shared_ptr<Socket> clientSocket(new asio::ip::tcp::socket(ioService));
+        acceptor.async_accept(*clientSocket.get(), [this, clientSocket](const boost::system::error_code& error)
+        {
+            if(error != 0) /* TODO: use throw */
+                std::cerr<<"Socket API Error:"<<error.message()<<"("<<error.value()<<")"<<std::endl;
+            else {
+                registerClient(clientSocket);
+
+                if(isStopped.load())
+                    acceptor.close();
+                else
+                    acyncAccept();
+            }
+        });
+    }
+
+    std::atomic<bool> isStopped;
+    asio::io_service& ioService;
+    asio::ip::tcp::acceptor acceptor;
+
+public:
+    /*TODO: move to ControlThread */
+    std::vector<std::shared_ptr<Socket>> connectedClients;
 };
 
 class ControlThread {
 public:
-    void start(){
+    void start(unsigned short port){
+        listener.reset(new ListeningSocket(ioService, port));
 
+        registerMyPipe();
+        listener->start();
+        ioService.run();
+
+        /*TODO: remove*/
+        std::cout<<"clientList:\n";
+        for(auto &e : listener->connectedClients) {
+            std::cout<<"client: "<<e->remote_endpoint()<<"\n";
+        }
     }
 
-private:
-    std::vector<ClientConnection> sessions;
+    /* may be called by other thread */
+    void stop(){
+        listener->stop();
+        ioService.stop();
+    }
 
+    PipeWriter &getPipeWriter(){ /* TODO: remove */
+        return pipe.writer();
+    }
+
+    /* TODO: change to private */
+    void registerMyPipe() {
+        bufSize = 1024;
+        totalBytesRead = 0;
+        pipeReadBuf.reset(new char[bufSize]);
+
+        asio::posix::stream_descriptor readEnd(ioService, pipe.reader().getFD());
+        readEnd.async_read_some(asio::buffer(pipeReadBuf.get(), bufSize), [this, &readEnd]
+                (const boost::system::error_code& error, std::size_t byteTransferred){
+            if( error != 0 ){
+                std::cerr<<"Pipe API Error:"<<error.message()<<"("<<error.value()<<")"<<std::endl;
+            }
+
+            totalBytesRead += byteTransferred;
+
+            if (totalBytesRead == byteTransferred)
+                return;
+
+            std::string contents(pipeReadBuf.get());
+            std::cout<< contents <<", byteTransferred: "<<byteTransferred<<std::endl;
+        });
+    }
+
+    asio::io_service ioService;
+
+private:
+    std::unique_ptr<ListeningSocket> listener;
+    Pipe pipe;
+
+    /*asio::streambuf pipeReadBuf;*/
+    std::unique_ptr<char[]> pipeReadBuf;
+    std::size_t totalBytesRead;
+    unsigned int bufSize;
 };
 
 
--- tests/ControlThreadTest.cpp
+++ tests/ControlThreadTest.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <Pipe.hpp>
+#include <ControlThread.hpp>
 #include "gtest/gtest.h"
 #include "boost/asio.hpp"
 
@@ -10,58 +11,59 @@ public:
     void connect(const char *ipAddr, int port){
         asio::ip::tcp::endpoint ep(asio::ip::address::from_string("127.0.0.1"), port);
         asio::io_service ios;
-        asio::ip::tcp::socket sock(ios, ep.protocol());
-        sock.connect(ep);
-        std::cout<<"Client: I'm connected!\n";
-        std::string buf = "Hello, world.";
-        sock.send(asio::buffer(buf));
+        sock.reset(new asio::ip::tcp::socket(ios, ep.protocol()));
+        sock->connect(ep);
     }
+
+    void sendString(std::string message) {
+        sock->send(asio::buffer(message));
+    }
+
+    void closeConnection() {
+        sock->close();
+    }
+private:
+    std::unique_ptr<asio::ip::tcp::socket> sock;
 };
 
 TEST(ControlThread, testAccept) {
-    unsigned short portNumOfServer = 3333;
-
+    unsigned short portNumOfServer = 33335;
     asio::ip::tcp::endpoint epServer(asio::ip::address_v4::any(), portNumOfServer);
     asio::io_service ios;
-
     asio::ip::tcp::acceptor acceptor(ios, epServer.protocol());
-
     boost::system::error_code ec;
-
     acceptor.bind(epServer, ec);
     acceptor.listen();
-
     asio::ip::tcp::socket sock(ios);
+    std::stringstream ss;
 
     std::thread client([](){
         DummyClient client;
-        client.connect("127.0.0.1", 3333);
+        client.connect("127.0.0.1", 33335);
+        client.sendString(std::string("Hello, world."));
+        client.closeConnection();
     });
 
-
-
-
-    ///////////
-    acceptor.async_accept(sock, [&sock](const boost::system::error_code& error){
+    acceptor.async_accept(sock, [&sock, &ss](const boost::system::error_code& error){
         if (!error){
             const int receiveBufSize = 1024;
             char receiveBuf[receiveBufSize];
 
-            std::cout << "async accept success!" << std::endl;
-            sock.async_receive(asio::buffer(receiveBuf), [&receiveBuf](const boost::system::error_code& error, std::size_t byteTransferred){
-                std::cout << "Message arrived from the connected client: " << receiveBuf << std::endl;
+            sock.async_receive(asio::buffer(receiveBuf), [&receiveBuf, &ss](const boost::system::error_code& error, std::size_t byteTransferred){
+                ss << "Message arrived from the connected client: " << receiveBuf << std::endl;
             });
         }
     });
 
     ios.run();
     /* TODO: move async_receive here. why not working? */
-    /////////////////////////
 
+    EXPECT_EQ(std::string("Message arrived from the connected client: Hello, world.\n"), ss.str());
 
+    ios.run();
+    sock.close();
+    acceptor.close();
     client.join();
-//    EXPECT_EQ(ec, 0);
-//    EXPECT_EQ(connectedSocketPort, ephemeralPort);
 }
 
 TEST(ControlThread, testPipeAsAsioStreamDescriptor) {
@@ -73,20 +75,96 @@ TEST(ControlThread, testPipeAsAsioStreamDescriptor) {
 
     //// write ////
     std::string buf = "Hello, world.";
-//    writeEnd.write_some(asio::buffer(buf));
+    // writeEnd.write_some(asio::buffer(buf));
     writeEnd.async_write_some(asio::buffer(buf), [](const boost::system::error_code& error, std::size_t byteTransferred){
         std::cout<<"write success!!!! "<<byteTransferred<<std::endl;
     });
 
-
     //// read ////
     const int receiveBufSize = 1024;
     char receiveBuf[receiveBufSize];
-//    readEnd.read_some(asio::buffer(receiveBuf));
+    // readEnd.read_some(asio::buffer(receiveBuf));
     readEnd.async_read_some(asio::buffer(receiveBuf), [&receiveBuf](const boost::system::error_code& error, std::size_t byteTransferred){
         std::cout<<receiveBuf<<", byteTransferred: "<<byteTransferred<<std::endl;
     });
 
     ios.run();
+}
 
+TEST(ControlThread, testMultipleClient) {
+    ControlThread cthr;
+    std::thread controlThread(std::bind(&ControlThread::start, &cthr, 33334));
+
+    auto clientRoutine = [](){
+        DummyClient client;
+        client.connect("127.0.0.1", 33334);
+    };
+
+    std::vector< std::unique_ptr<std::thread> > clients;
+    for(int i = 0; i < 3; i++){
+        std::unique_ptr<std::thread> th(new std::thread(clientRoutine));
+        clients.push_back(std::move(th));
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    cthr.stop();
+
+    controlThread.join();
+    for (auto& th : clients)
+        th->join();
+}
+
+struct PipeInputStream {
+    PipeInputStream(asio::io_service &ioService, int readFD) :
+            totalBytesRead(0),
+            readPipe(new asio::posix::stream_descriptor(ioService, readFD)) {}
+
+    std::shared_ptr<asio::posix::stream_descriptor> readPipe;
+    asio::streambuf buf;
+    std::size_t totalBytesRead;
+};
+
+void pipeReadCallback(const boost::system::error_code& ec, std::size_t bytesRead, std::shared_ptr<PipeInputStream> pis) {
+    if (ec != 0) {
+        std::cerr<<"Pipe API Error:"<<ec.message()<<"("<<ec.value()<<")"<<std::endl;
+        return;
+    }
+
+    pis->totalBytesRead += bytesRead;
+
+    std::istream is(&pis->buf);
+    std::string contents;
+//    is >> contents;
+    std::getline(is, contents);
+    std::cout<< "received:"<< contents<<", byteRead: "<<bytesRead<<"\n";
+
+//    pis->readPipe->async_read_some(asio::buffer(pis->buf.get() + pis->totalBytesRead,
+//                                                pis->bufSize - pis->totalBytesRead),
+//                                   std::bind(pipeReadCallback,
+//                                             std::placeholders::_1,
+//                                             std::placeholders::_2, pis));
+}
+
+TEST(ControlThread, testReadPipeOfControlThread) {
+    Pipe pipe;
+    asio::io_service ios;
+
+    //// write ////
+    std::string buf = "Hello, world.";
+    asio::posix::stream_descriptor writeEnd(ios, pipe.writer().getFD());
+    writeEnd.write_some(asio::buffer(buf));
+
+    std::string buf2 = "Hello, cthr.";
+    pipe.writer().writeBytes(buf2.c_str(), buf2.length());
+
+    //// read ////
+    std::shared_ptr<PipeInputStream> pis(new PipeInputStream(ios, pipe.reader().getFD()));
+
+    boost::asio::streambuf::mutable_buffers_type mutableBuffer = pis->buf.prepare(1024);
+    pis->readPipe->async_read_some(asio::buffer(mutableBuffer),
+                                   std::bind(pipeReadCallback,
+                                             std::placeholders::_1,
+                                             std::placeholders::_2, pis));
+
+    ios.run();
 }
